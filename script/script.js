1cc736972c24ffa38bb3cd4335309822bbf6f7f9
sasha-belokur <alex.belokur17@gmail.com>
2017-09-14 11:31:14 +0300

    functions open and close modal window

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let modalWindow = document.querySelector('.modal-window-write-us');
+let openBtn = document.querySelector('.contacts .btn');
+let closeBtn = document.querySelector('.modal-window-write-us .close-window')
+
+
+openBtn.addEventListener('click', openModalWindow);
+closeBtn.addEventListener('click', closeModalWindow);
+
+function openModalWindow() {
+	event.preventDefault();
+	modalWindow.classList.add('modal-window-show');
+}
+
+function closeModalWindow() {
+	event.preventDefault();
+	modalWindow.classList.remove('modal-window-show');
+}
