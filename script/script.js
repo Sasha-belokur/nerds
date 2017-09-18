54b3689f3ec3d66518ee2626602c91ea6e6ebaad
Alex Belokur <alex.belokur17@gmail.com>
2017-09-18 19:40:29 +0300

    minor changes with main navigation and catalog.html

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 let modalWindow = document.querySelector('.modal-window-write-us');
 let openBtn = document.querySelector('.contacts .btn');
-let closeBtn = document.querySelector('.modal-window-write-us .close-window')
+let closeBtn = document.querySelector('.modal-window-write-us .close-window');
 
 
 openBtn.addEventListener('click', openModalWindow);
 closeBtn.addEventListener('click', closeModalWindow);
+
 
 function openModalWindow(event) {
 	event.preventDefault();
